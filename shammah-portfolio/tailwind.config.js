f633563e047920e7b5b566f04fa35f59ea2630f2
Shammah Dzwairo <shammah.dzwairo@ashesi.edu.gh>
2025-04-26 21:19:42 +0000

    fixed errors in packages installation

---
+++ shammah-portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // Add all file extensions you use
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
